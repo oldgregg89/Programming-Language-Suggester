3c4ef88a100f1bc61f1cf5e802881bdd3173c473
Ian Gregg <iangregg188@gmail.com>
2020-06-05 12:59:05 -0700

    worked on javascript structure, formated styles.css to correct the imag issue with results, addeed another question in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+
+const fridgeProblem = function (){
+  if
+}
+
+const aliensProblem = function (){
+  if
+}
+
+const planeteProblem = function (){
+  if
+}
+
+const value = function (){
+  if
+}
+
+
 $(document).ready(function(){
-  //working on this right after html
+  $("form#programPerson").submit(event){}
+  const go = 
+  const javaS
+  const python
+  const ruby 
+  const rust
+  const swift
+  const sum = fridge + aliens + planet
+
+    if () {
+      $(".programLang1").show();
+      $(".programLang2").show();
+      $(".programLang3").show();
+      $(".programLang4").show();
+      $(".programLang5").show();
+      $(".programLang6").show();
+    }
+  });
 });
